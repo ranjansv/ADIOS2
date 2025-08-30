cc598693b52e1600b97cbe785552cb52f243997f
Ranjan Sarpangala Venkatesh <ranjansv@gatech.edu>
2025-08-30 23:09:16 +0000

    Fix: Create unique OID for DAOS KV

--- source/adios2/engine/daos/DaosWriter.cpp
+++ source/adios2/engine/daos/DaosWriter.cpp
@@ -2329,6 +2329,12 @@ void DaosWriter::CreateDaosKVObject() {
     int rc;
     //rc = daos_obj_generate_oid(coh, &oid, DAOS_OT_KV_HASHED, OC_SX, 0, 0);
     //rc = daos_obj_generate_oid(coh, &oid, DAOS_OF_KV_FLAT, OC_S1, 0, 0);
+    oid.hi = 0;
+    // Use a random number generator for oid.lo instead of getpid()
+    std::random_device rd;
+    std::mt19937_64 gen(rd());
+    oid.lo = gen();
+
     rc = daos_obj_generate_oid(coh, &oid, DAOS_OT_KV_HASHED, 0, 0, 0);
     //rc = daos_obj_generate_oid(coh, &oid, DAOS_OT_KV_HASHED, OC_RP_2GX, 0, 0);
     ASSERT(rc == 0, "daos_obj_generate_oid failed with %d", rc);
